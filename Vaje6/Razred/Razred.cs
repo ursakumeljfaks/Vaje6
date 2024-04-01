bb034231e33254bd62ae3033298df0501852764a
ursakumelj <uk6584@student.uni-lj.si>
2024-04-01 16:56:35 +0200

    razred razred2 sln

---
+++ Vaje6/Razred/Razred.cs
@@ -0,0 +1,9 @@
+﻿namespace Razred;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ Vaje6/Razred2/Razred2.cs
@@ -0,0 +1,9 @@
+﻿namespace Razred2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
